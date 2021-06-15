c815199e05507d758286fd8a85892f024c8e7564
crazyjhun09 <51357216+crazyjhun09@users.noreply.github.com>
2021-06-15 17:38:58 +0800

    c

--- feed_files/submit.php
+++ feed_files/submit.php
@@ -16,23 +16,41 @@
 		$ingsAmt=$_POST["ingAmt"];
 		$amt = array_merge(...$ingsAmt);
 
-
 		$categ=$_POST["categ"];
-
+		if(!isset($ingsArray)){
+			$sql="INSERT INTO ".$categ."(".$categ."_name, ".$categ."_amt, food_id) VALUES (null, null, '$fID')";
+			mysqli_query($con,$sql);
+		}
 
 		foreach (array_combine($ing, $amt) as $ing => $amt) {
 
 
 			$ings = strval($ing);
 			$amts = strval($amt);
+
 			$sql="INSERT INTO ".$categ."(".$categ."_name, ".$categ."_amt, food_id) VALUES ('$ings', '$amts', '$fID')";
 		
 			$result = mysqli_query($con,$sql);
+			if($result){
+				$response["success"] = 1;
+				$response["message"] = "New user successfully created.";
+			
+				// echoing JSON response
+				echo json_encode($response);
+				}
+				else
+				{
+					$response["success"] = 0;
+					$response["message"] = 'Database error ' . mysqli_errno($con) . ' ' . mysqli_error($con);
+				
+					// echoing JSON response
+					echo json_encode($response);
+				}
 			$sql2="SELECT * FROM ingredients_all WHERE ing_name ='$ings'";
 			$result1 = $con -> query($sql2);
+
 			if($result1-> num_rows == 0){
 				$sqlInsert = "INSERT INTO ingredients_all (ing_name) VALUE ('$ings')";
-				mysqli_query($con,$sqlInsert);
 			}
 		}
 ?>
